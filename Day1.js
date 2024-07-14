0b59cf5da1b25a9edfc71a4967c2b4e5251d1da6
Dipu Sah <sahd7929@gmail.com>
2024-07-14 10:27:09 +0530

    Day 2 challange completed

---
+++ Day1.js
@@ -0,0 +1,37 @@
+//Activity 1 variable declaration
+
+var num
+num=40
+
+let myname
+myname="Dipu"
+
+console.log(num);
+console.log(myname);
+
+//Activity 2 const declaration
+const boolValue=true
+console.log(boolValue);
+
+//Activity 3 data types
+let numb=10
+let str="abc"
+let booleanValue=false
+let nullValue=null
+let undef=undefined
+let obj={
+    name:"Dipu",
+    age:23
+}
+let arr=[1,2,"Hello"]
+
+console.log(typeof num,typeof str,typeof booleanValue,typeof nullValue,typeof undef,typeof obj,typeof arr);
+
+//Activity 4 reassigning
+let first=20
+console.log(first);
+first=30
+console.log(first);
+
+//Activit 5 observer error while reassign to const
+boolValue=false
---
+++ Day2.js
@@ -0,0 +1,24 @@
+// Activity 1 Arithmetic Operations
+let a = 20, b= 7,c=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Activity 2 Assignment Operators
+console.log(a+=b);
+console.log(a-=b);
+
+//Activity 3 Comparision Operators
+console.log(a<b , a>b);
+console.log(a<=c , a>=b);
+console.log(a==c , a===c);
+
+// Activity 4 Logical Operators
+console.log(a<b && a>b);
+console.log(a<=c || a>=b);
+console.log(!true);
+
+//Activity 5 Ternary Operator
+console.log(a>0?'positive':'negative');
