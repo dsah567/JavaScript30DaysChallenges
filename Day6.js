08c2bae15e17ca202ebe4474a39de4451843788a
Dipu Sah <sahd7929@gmail.com>
2024-07-18 19:54:45 +0530

    Day 6 array completed

---
+++ Day6.js
@@ -0,0 +1,38 @@
+//Activity 1 Arrays
+let a = [1,2,3,4,5]
+// console.log(a);
+
+// console.log(a[0],a[4]);
+
+// //Activity 2 Array method Basic
+// a.push(6)
+// console.log(a);
+// a.pop()
+// console.log(a);
+// a.shift()
+// console.log(a);
+// a.unshift(0)
+// console.log(a);
+
+// //Array method Intermediate
+// let b=a.map((n)=>n*2)
+// console.log(b);
+// let c=a.filter((n)=>n%2==0)
+// console.log(c);
+
+// let d=a.reduce((acc,current)=>acc+current)
+// console.log(d);
+
+// //activity 4 Array Iteration 
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+// a.forEach(element => {
+//     console.log(element);
+// });
+
+//Activity 5 Multi-dimensional array
+let aa=[[1,2,3],[4,5,6]]
+console.log(aa);
+console.log(aa[1][2]);
