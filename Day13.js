2ba36877b3c447d0e4220deba6eac2d451e23e7b
Dipu Sah <sahd7929@gmail.com>
2024-08-03 01:58:48 +0530

    Day 13 modules

--- Day13.js
+++ Day13.js
@@ -1,5 +1,15 @@
-import add from "./Day13Module.js";
-console.log(add(1,7));
+// import add from "./Day13Module.js";
+// console.log(add(1,7));
+
+// import person from "./Day13Module.js";
+// console.log(person.myname);
+// person.intro()
 
 
-//Left till understanding webpack
+
+// import def from "./Day13Module.js";
+// import {greet,lol} from "./Day13Module.js";
+// def()
+// greet()
+// lol()
+
--- Day13Module.js
+++ Day13Module.js
@@ -1,5 +1,29 @@
-let add=(a,b)=>{
-    return a+b
-};
+// let add=(a,b)=>{
+//     return a+b
+// };
 
-export default add;
+// export default add;
+
+// let person={
+//     myname : "Dipu",
+//     intro(){
+//         console.log(`Hello ${this.myname}`)
+//     }
+// }
+
+// export default person
+
+function greet(){
+    console.log("greet");
+}
+
+function lol(){
+    console.log("lol");
+}
+
+function def() {
+    console.log("def");
+}
+
+export default def
+export {greet,lol}
