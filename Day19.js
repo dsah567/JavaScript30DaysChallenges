4d3c2309e6513fbb8027fc8db159410c0965aa04
Dipu Sah <sahd7929@gmail.com>
2024-08-04 03:05:25 +0530

    day 19 regex

---
+++ Day19.js
@@ -0,0 +1,36 @@
+//Activity 1 Basic Regular Expression 
+let str= `What is JavaScript ? H sahd7929@gmail.com  dipu123@rr.com. https://courses.chaicode.com/
+JavaScript is a 9ligheetweight,av5 cross-platform, 1 single-threaded412, and iytrtnterpreted compiled programming language. It is also known as the scripting language for webpages. It is well-known for the development of web pages, and many non-browser environments also use it.
+JavaScript is a weakly typed22 language (dynamically typed). 14 JavaScript can be used for Client-side developments as well as Server-side developments. JavaScript is both an imperative and declarative type of language. JavaScript contains a standard library of objects, like Array, Date, and Math, and a core set of language elements like operators, control structures, and statements`
+
+// let re= /javascript/gi
+// let found = str.match(re)
+// console.log(found);
+
+// let renum=/[0-9]/g
+// let funnum= str.match(renum)
+// console.log(funnum);
+
+//Activity 2 character Classes and Quantifiers
+// let re= /[A-Z][a-zA-Z]*\S/g
+// let found = str.match(re)
+// console.log(found);
+
+// let re= /\d+/g
+// let found = str.match(re)
+// console.log(found);
+
+// Activity 3 Grouping and Capturing 
+// let re= /[a-z0-9]+@[a-z]+\.com*\S/g
+// let found = str.match(re)
+// console.log(found);
+
+//Activity 4 Assertion and Boundaries 
+// let re= /^[a-zA-Z0-9]/g
+// let found = str.match(re)
+// console.log(found);
+
+// let re= /[a-zA-Z0-9]$/g
+// let found = str.match(re)
+// console.log(found);
+
